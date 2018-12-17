5a7e1140e17a622eef700b4172f14567e595314e
Алексей Бельченков <u608110@gmail.com>
2018-12-17 16:17:17 +0400

    GraphQL + MongoDb

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -40,24 +42,45 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+                .then(events => events.map(event => {
+                    return {
+                        ...event._doc,
+                        _id: event.id
+                    };
+                }))
+                .catch(err => {
+                    console.error(err);
+                })
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
+                date: new Date(args.eventInput.date)
+            });
 
-            events.push(event);
-            return event;
+            return event.save()
+                .then(result => {
+                    console.log(result);
+                    return {
+                        ...result._doc,
+                        _id: event._doc._id.toString()
+                    };
+                })
+                .catch(err => {
+                    throw err;
+                });
         }
     },
     graphiql: true
 }));
 
-app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-});
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-1owsl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`Server running on port 3000`);
+        });
+    })
+    .catch(err => console.error(err));
